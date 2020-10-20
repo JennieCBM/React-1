d49e58ecee32036303a13115f513bddc20fa5a4d
Jennie <jenniebuitrago@gmail.com>
2020-10-20 11:54:57 -0300

    Updates

---
+++ precache-manifest.285e272dc2f2409d1e591465daf42488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567621cecb15e96702f26a1d2fb0b707",
+    "url": "/React-1/index.html"
+  },
+  {
+    "revision": "f2d4b2b1c02611e613bc",
+    "url": "/React-1/static/css/main.d43a3b2d.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/React-1/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-1/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d4b2b1c02611e613bc",
+    "url": "/React-1/static/js/main.21a6949f.chunk.js"
+  },
+  {
+    "revision": "dca5764546226056f5ac",
+    "url": "/React-1/static/js/runtime-main.134b1286.js"
+  }
+]);
